4017ae5a87bdddacba7a5b2f1bdc5c2ad08a5956
sabina0407 <sabinaruzieva04@gmail.com>
2025-07-28 18:41:34 -0400

    Week 3- state and props added

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,17 @@
+import TodoListItem from "./TodoListItem";  
+
 function TodoList() {
       const todos = [
     { id: 1, title: 'review resources' },
     { id: 2, title: 'take notes' },
     { id: 3, title: 'code out app' },
-  ]
+  ];
     return (
-        <div>
-            <ul>
+          <ul>
             {todos.map((todo) => (
-            <li key={todo.id}>{todo.title} </li>
+            <TodoListItem key={todo.id} todo={todo} />
             ))}
-        </ul>
-        </div>
+          </ul>
     );
 }
 
